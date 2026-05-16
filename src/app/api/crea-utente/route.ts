de21ba0899d786634f2da8cfb29beb050fe99940
Martins <martinsosem@gmail.com>
2026-05-16 19:21:03 +0200

    Fix API route dynamic

--- src/app/api/crea-utente/route.ts
+++ src/app/api/crea-utente/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { NextRequest, NextResponse } from 'next/server'
 import { initializeApp, getApps, cert } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
